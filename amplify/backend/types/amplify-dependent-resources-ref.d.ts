9fb9a0b5e63bf1d0ef0d62aa0486dbb4f7ad408d
Caleb Sides <calebsides@calebs-mbp.lan>
2023-09-04 02:45:58 -0400

    WIP adding api

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "rapgpt": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "rapgpt9f982131": {
       "AppClientID": "string",
